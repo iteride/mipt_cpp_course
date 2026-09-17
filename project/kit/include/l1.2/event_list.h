4b15f7392ba36442a670ab6e60caa68b608ff5be
unknown <timoha0.9ok@gmail.com>
2026-09-17 21:56:35 +0300

    correcting event.h

--- project/kit/include/l1.2/event_list.h
+++ project/kit/include/l1.2/event_list.h
@@ -72,6 +72,8 @@ void ListPopFront(EventList* list);
 // Освобождает всё. После вызова список пуст и пригоден к использованию снова.
 void ListClear(EventList* list);
 
+void EventClear(Event* out);
+
 }  // namespace nano_edr
 
 #endif  // NANO_EDR_KIT_EVENT_LIST_H
